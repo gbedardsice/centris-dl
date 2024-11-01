228fe6b64112680486bcabbee4fb94382c24e254
Gabriel Bédard Sicé <gbedardsice@gmail.com>
2024-11-01 14:32:26 -0400

    Just generate zip file

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+#!/usr/bin/env node
+
 import { Builder, By, until } from "selenium-webdriver";
 import chrome from "selenium-webdriver/chrome.js";
-import * as fs from "fs";
+import fs from "fs-extra";
 import * as path from "path";
 import * as mime from "mime-types";
 import { URL } from "url";
+import { execSync } from "child_process";
 
 async function extractInformation(url) {
   const driver = await new Builder()
@@ -13,14 +16,12 @@ async function extractInformation(url) {
   try {
     await driver.get(url);
 
-    const dirName = await driver
+    const listingName = await driver
       .findElement(By.css(".house-info h2"))
       .getText();
-    const outputDir = path.join(process.cwd(), "output", dirName);
+    const outputDir = path.join(process.cwd(), "tmp", listingName);
 
-    if (fs.existsSync(outputDir)) {
-      fs.rmSync(outputDir, { recursive: true });
-    }
+    fs.removeSync(outputDir);
     fs.mkdirSync(outputDir, { recursive: true });
 
     const houseInfo = await driver.findElement(By.css(".house-info")).getText();
@@ -49,10 +50,10 @@ async function extractInformation(url) {
       return elements.length === 0;
     }, 10000);
 
-    const eContainer = await driver.findElement(
+    const galleryTrigger = await driver.findElement(
       By.css(".primary-photo-container a")
     );
-    await eContainer.click();
+    await galleryTrigger.click();
 
     await driver.wait(until.elementLocated(By.css(".carousel img")), 10000);
 
@@ -78,21 +79,21 @@ async function extractInformation(url) {
       srcThumb.searchParams.set("w", "1260");
       srcThumb.searchParams.set("h", "1024");
       const srcFull = srcThumb.toString();
-
-      let contentType;
-      const response = await fetch(srcFull).then((res) => {
-        contentType = res.headers.get("content-type");
-        return res.arrayBuffer();
-      });
+      const response = await fetch(srcFull);
+      const contentType = response.headers.get("content-type");
+      const buffer = await response.arrayBuffer();
       const ext = mime.extension(contentType);
       const cleanTitle = title.replace(/[\\/:"*?<>|]+/g, "");
       const baseFilename = `${i}-${cleanTitle}`;
       const dst = path.join(outputDir, `${baseFilename}.${ext}`);
 
-      fs.writeFileSync(dst, Buffer.from(response));
+      fs.writeFileSync(dst, Buffer.from(buffer));
     });
 
     await Promise.all(promises);
+
+    const archive = execSync(`zip -r "${listingName}.zip" "${outputDir}"`);
+    fs.removeSync(outputDir);
   } finally {
     await driver.quit();
   }
